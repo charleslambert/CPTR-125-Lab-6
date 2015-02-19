83fccde621a20898e7488bf3117177f107911aac
Charles Lambert <c.lambert1213@gmail.com>
2015-02-18 21:10:54 -0800

    BowlingGame.rb

--- BowlingGame.rb
+++ BowlingGame.rb
@@ -1,12 +1,10 @@
 class Game
 	def initialize 
-		@score = 0
 		@rolls = [0]*21
 		@currentRoll = 0
 	end
 	
 	def roll(pins)
-		@score += pins
 		@rolls[@currentRoll += 1] = pins
 	end
 	
@@ -15,7 +13,7 @@ class Game
 		for i in (0...@rolls.length)
 			score += @rolls[i]
 		end
-		return @score 
+		return score 
 	end
 	
 end
