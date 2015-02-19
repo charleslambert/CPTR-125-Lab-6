3048ca47703904123ca1cdfa50c40d9b4af14bb3
Charles Lambert <c.lambert1213@gmail.com>
2015-02-18 21:57:14 -0800

    BowlingGame.rb

--- BowlingGame.rb
+++ BowlingGame.rb
@@ -5,20 +5,22 @@ class Game
 	end
 	
 	def roll(pins)
-		@rolls[(@currentRoll += 1)] = pins
-		puts @rolls
+		@rolls[@currentRoll] = pins
+		@currentRoll +=1
+		print @rolls
+		puts ""
 	end
 	
 	def score()
 		score = 0
-		i = 1
+		frameIndex = 0
 		for frame in (0...10)
-			if (@rolls[i] +@rolls[i + 1] == 10) #spare
-				score += (10 +@rolls[i+2])
-				i += 2
+			if (@rolls[frameIndex] + @rolls[frameIndex +1] ==10) #spare
+				score += 10 + @rolls[frameIndex + 2]
+				frameIndex += 2
 			else
-				score += (@rolls[i] + @rolls[i + 1])
-				i += 2
+				score += @rolls[frameIndex] + @rolls[frameIndex + 1]
+				frameIndex +=2
 			end
 		end
 		return score 
